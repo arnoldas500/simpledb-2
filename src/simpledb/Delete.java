105de96cfd4dd60e5ad87dd6662267adc80e3b18
klobes5 <kevin.lobiondo@gmail.com>
2016-12-13 16:14:02 -0500

    Completed Delete class Added transaction id field "t", "afield", TupleDesc deleteDesc, and a boolean for if tuple was staged for deletion. Delete method initalizes an array containing deletions string and array of IntType as well as new tupleDesc which holds the string and type. ReadNext method looks at boolean for if tuple staged for deletion and if next looks up the transaction id "t" and deletes tuple. Sum counter holds the total number of tuples to be deleted and a tuple containing this sum is returned.

--- src/simpledb/Delete.java
+++ src/simpledb/Delete.java
@@ -1,4 +1,6 @@
-package simpledb;  
+package simpledb;
+
+import java.io.IOException;
 
 /**
  * The delete operator.  Delete reads tuples from its child operator and
@@ -13,28 +15,37 @@ public class Delete extends AbstractDbIterator {
      * @param child The child operator from which to read tuples for deletion
      */
 	
-	protected DbIterator child;
+	protected DbIterator child; //iterate through the children
 	protected TransactionId t;
 	protected int afield;
+	protected TupleDesc deleteDesc;  // the tupledesc is now defined in Delete Method operator
+	protected boolean wasDeleted; // boolean true or false if was deleted
+	//Predicate p; //don't think I need this
+	
 	
 	
     public Delete(TransactionId t, DbIterator child) {
         // some code goes here
     	this.child = child;
     	this.t = t;
+       	String[] nameArray = new String[] {"StagedforDeletion"};//array of deletions string
+    	Type[] typeArray = new Type[] {Type.INT_TYPE}; //type of deletions
+    	deleteDesc = new TupleDesc(typeArray, nameArray); 
     	
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
     	TupleDesc td = child.getTupleDesc();
-    	Type typeDes = td.getType(afield); //this line may not be necessary
+    	//Type typeDes = td.getType(afield); //this line may not be necessary
         return td;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
     	child.open();
+    	//on open initialize boolean to false
+    	wasDeleted= false;
     	
     }
 
@@ -59,6 +70,25 @@ public class Delete extends AbstractDbIterator {
      */
     protected Tuple readNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+    	
+    	int sum = 0; //sum is the integer in the tuple which contains the number of deleted records
+    	
+    	if (wasDeleted){
+    		return null; //case when tuple has been deleted we return nothing
+    	}
+    	
+    	while (child.hasNext())
+    	{
+    		Tuple myTuple = child.next();//get the next child and save 
+    
+    		//should below be in a try/catch block or will the "throws" be sufficient?
+    		Database.getBufferPool().deleteTuple(t, myTuple);    			
+    		
+    		sum++; // add to sum for each deleted tuple
+    	}
+    	Tuple set = new Tuple(deleteDesc); //set is a tuple containing the total number of tuples staged for deletion
+    	set.setField(0, new IntField(sum));
+    	wasDeleted = true;
+    	return set;
     }
 }
